37ff0ff72fb510be8a8db80bdba02940efa7e2ae
gkgkgkgk <gkepets@gmail.com>
2022-02-15 12:48:43 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# WebPlatform
+# Getting Started
+
+Before you get started, make sure you have the following things installed:
+* nodejs (I recommend 14+)
+* npm
+* git
+
+In order to clone the repository, run ```git clone git@github.com:FortyOneTraders/WebPlatform.git```
+
+Then, go into the directory, and run ```npm install```
+
+Then, run ```npm run dev```.
+
+Navigate to localhost:3000 in your browser, and you should see it running.
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+export default () => {
+  return (
+    <div>
+      <h1>Hello and welcome to the boilerplate for NextJS.</h1>
+      <hr />
+      <h3>Pages</h3>
+      <p>To create a page, add a folder in the pages directory that is named after the page you are making.</p>
+      <p>For example, if you are making an "About US" page, make a folder called AboutUs. Then, put a file called index.js in that directory, and you can start building thqat page!</p>
+      <hr />
+      <h3>APIs</h3>
+      <p>To create an API endpoint, add a file named after the endpoint in the api folder.</p>
+      <p>For example, if you are making an endpoint that returns the word hello, as seen in this boilerplate code, make a file called hello.js. This file will automatically be linked to the endpoint /api/hello.</p>
+      <hr />
+      <h3>Components</h3>
+      <p>To create a React component, add a folder with the name of the component in the components directory.</p>
+      <p>Its pretty much the same as a normal React app.</p>
+      <hr />
+    </div>
+  )
+}
