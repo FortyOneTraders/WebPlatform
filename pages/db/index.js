43ac477fdfa6d0a3ca1a62af55025473f64f96b6
Gregory Presser <gpress2222@gmail.com>
2022-02-16 03:09:19 -0500

    Started mongo

---
+++ pages/db/index.js
@@ -0,0 +1,18 @@
+import {useEffect, useState} from 'react'; 
+import axios from 'axios'; 
+
+export default ()=>{
+	const [data, setData] = useState(""); 
+
+	useEffect(()=>{
+		
+		(async()=>{
+				
+		})()
+	},[]);	
+
+	return (
+		<div>Test Endpoint {data}</div>
+	);
+
+}
