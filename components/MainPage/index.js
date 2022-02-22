8e97f3ba555be91cdce58c07b6113bbc5234f7e8
Effie Bluestone <effiebluestone@Effies-MacBook-Air.local>
2022-02-22 13:48:48 -0500

    Added Donate Button

--- components/MainPage/index.js
+++ components/MainPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './MainPage.module.css';
+import DonateButton from '../DonateButton';
 
 // I was thinking something like this: https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/602f2109a787c146dcbe2b66_601b1c1f7567a7399353fe47_traackr.jpeg
 
@@ -9,7 +10,9 @@ const MainPage = () => {
             <div className={styles.flexItem}><h1 className={styles.title}>Welcome to Peter Cooper Trading.</h1>
                 <p>Peter Cooper Trading is a fund associated with the Cooper Union FinTech club. This fund aims to serve as an educational experience for students who are interested in financial technology.</p></div>
             <div className={styles.flexItem}><img style={{ width: '100%' }} src=' https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/602f2109a787c146dcbe2b66_601b1c1f7567a7399353fe47_traackr.jpeg'></img></div>
-        </div>
+         
+        <div className={styles.flexItem}> <DonateButton className={styles.button}>This is a button</DonateButton></div>
+         </div>
     )
 }
 
